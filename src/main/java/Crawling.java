82fe53378241246b5a94e1d04ba69fc732cb461d
JinHoooooou <jinho4744@naver.com>
2020-09-21 16:39:01 +0900

    add Crawling.java

--- src/main/java/Crawling.java
+++ src/main/java/Crawling.java
@@ -1,5 +1,23 @@
-package PACKAGE_NAME;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class Crawling {
 
+  private static WebDriver webDriver;
+  private static String NAVER_URL = "https://www.naver.com";
+
+  public static void main(String[] args) {
+    connectChromeDriver();
+    webDriver.get(NAVER_URL);
+    System.out.println(webDriver.getTitle());
+  }
+
+  private static void connectChromeDriver() {
+    WebDriverManager.chromedriver().setup();
+    ChromeOptions options = new ChromeOptions();
+    options.addArguments("--headless");
+    webDriver = new ChromeDriver(options);
+  }
 }
