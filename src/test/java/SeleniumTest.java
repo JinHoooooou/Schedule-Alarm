e1d207202e025b29dd1bd04dfa7c252ab74b65b7
JinHoooooou <jinho4744@naver.com>
2020-09-24 17:17:10 +0900

    필요없는 테스트 삭제

--- src/test/java/SeleniumTest.java
+++ src/test/java/SeleniumTest.java
@@ -4,16 +4,5 @@ import org.junit.Test;
 
 public class SeleniumTest {
 
-  @Test
-  public void testWhenUrlIsMelon() {
-    // Given: Set melon url
-    String url = "https://www.melon.com/index.htm";
 
-    // When: Call getWebPageTitle method
-    String actual = new Selenium().getWebPageTitle(url);
-
-    // Then: Should return "Melon::음악이 필요한 순간, 멜론"
-    String expected = "Melon::음악이 필요한 순간, 멜론";
-    assertEquals(expected, actual);
-  }
 }
