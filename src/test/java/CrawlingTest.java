3b00f99789d25691f5af13439476a82af6dd9a8a
JinHoooooou <jinho4744@naver.com>
2020-09-24 16:48:07 +0900

    오늘 경기 일정 없을때 테스트 추가

--- src/test/java/CrawlingTest.java
+++ src/test/java/CrawlingTest.java
@@ -7,6 +7,7 @@ import org.junit.Test;
 public class CrawlingTest {
 
   private static final String NAVER_KBO_SCHEDULE = "https://search.naver.com/search.naver?ie=UTF-8&query=kbo%EC%9D%BC%EC%A0%95&sm=chr_hty";
+  private static final String URL_OF_NOT_HAVE_MATCH = "https://search.naver.com/search.naver?sm=tab_hty.top&where=nexearch&query=9%EC%9B%94+28%EC%9D%BC+kbo%EC%9D%BC%EC%A0%95&oquery=2020%EB%85%84+9%EC%9B%94+29%EC%9D%BC+%EC%95%BC%EA%B5%AC+%EA%B2%BD%EA%B8%B0%EC%9D%BC%EC%A0%95&tqi=U2xK1sprvTVsseMA62NssssssKG-318856";
 
   @Test
   public void testShouldReturnKboScheduleString() throws IOException {
@@ -21,5 +22,12 @@ public class CrawlingTest {
             + "18:30, 롯데(박세웅) vs 한화(서폴드), 대전\n"
             + "18:30, 삼성(라이블리) vs 두산(최원준), 잠실\n";
     assertEquals(expected, actual);
+  }
+
+  @Test
+  public void testWhenTodayNotHaveMatch() throws IOException {
+    String actual = new Crawling().crawlKboSchedule(URL_OF_NOT_HAVE_MATCH);
+
+    assertEquals("오늘 경기 일정은 없습니다.", actual);
   }
 }
