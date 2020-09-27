5586312d437af9a57ae47781a98fb876eaac053d
JinHoooooou <jinho4744@naver.com>
2020-09-27 17:58:06 +0900

    네이버 로그인 함수 추가

--- src/main/java/Selenium.java
+++ src/main/java/Selenium.java
@@ -1,5 +1,11 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
+import java.awt.Toolkit;
+import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.StringSelection;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
@@ -7,10 +13,8 @@ public class Selenium {
 
   private static WebDriver webDriver;
 
-  public String getWebPageTitle(String url) {
+  public Selenium() {
     connectChromeDriver();
-    webDriver.get(url);
-    return webDriver.getTitle();
   }
 
   public static void main(String[] args) {
@@ -22,5 +26,41 @@ public class Selenium {
     ChromeOptions options = new ChromeOptions();
     options.addArguments("--headless");
     webDriver = new ChromeDriver(options);
+  }
+
+  public void login(String url) throws InterruptedException {
+    webDriver.get(url);
+    Thread.sleep(1000);
+    System.out.println(webDriver.getTitle());
+    inputNaverId(System.getenv("NAVER_ID"));
+    inputNaverPassword(System.getenv("NAVER_PASSWORD"));
+    webDriver.findElement(By.id("log.login")).submit();
+    Thread.sleep(1000);
+    System.out.println(webDriver.getTitle());
+  }
+
+  public void inputNaverPassword(String naverPassword) throws InterruptedException {
+    copyToClipBoard(naverPassword);
+
+    WebElement pw = webDriver.findElement(By.id("pw"));
+    pw.click();
+    pw.sendKeys(Keys.CONTROL + "v");
+
+    Thread.sleep(1000);
+  }
+
+  public void inputNaverId(String naverId) throws InterruptedException {
+    copyToClipBoard(naverId);
+
+    WebElement id = webDriver.findElement(By.id("id"));
+    id.click();
+    id.sendKeys(Keys.CONTROL + "v");
+
+    Thread.sleep(1000);
+  }
+
+  public void copyToClipBoard(String contents) {
+    Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
+    clipboard.setContents(new StringSelection(contents), null);
   }
 }
