9423071a8be902f34b2231354ebb82274f9c7d96
JinHoooooou <jinho4744@naver.com>
2020-09-27 17:56:15 +0900

    네이버 로그인 테스트 추가

--- src/test/java/SeleniumTest.java
+++ src/test/java/SeleniumTest.java
@@ -1,8 +1,20 @@
-import static org.junit.Assert.*;
-
+import org.junit.Before;
 import org.junit.Test;
 
 public class SeleniumTest {
 
+  private static final String NAVER_LOGIN_URL = "https://nid.naver.com/nidlogin.login?mode=form&url=https%3A%2F%2Fwww.naver.com";
+
+  private Selenium selenium;
+
+  @Before
+  public void setUp() {
+    selenium = new Selenium();
+  }
+
+  @Test
+  public void seleniumLoginTest() throws InterruptedException {
+    selenium.login(NAVER_LOGIN_URL);
+  }
 
 }
